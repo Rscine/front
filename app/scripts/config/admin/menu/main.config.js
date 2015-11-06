4e0d9a365e0b67e6340b81d187c757c09bc076c5
monzey <maxi.bertrand@gmail.com>
2015-11-06 22:49:01 +0100

    added menu configuration structure

---
+++ app/scripts/config/admin/menu/main.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rscineFrontendApp.config:main
+ * @description
+ * Configuration of ng-admin menu
+ */
+
+angular.module('rscineFrontendApp')
+
+    .config(function(NgAdminConfigurationProvider, adminProvider) {
+        var nga = NgAdminConfigurationProvider;
+
+        var admin = adminProvider.getApplication();
+
+        var menu = nga.menu()
+            .addChild(
+                nga.menu().title('Users general')
+                .addChild(nga.menu(adminProvider.getEntity('users')))
+                .addChild(nga.menu(adminProvider.getEntity('customers')))
+                .addChild(nga.menu(adminProvider.getEntity('contractors')))
+            )
+            .addChild(
+                nga.menu(adminProvider.getEntity('departments'))
+            )
+            .addChild(
+                nga.menu(adminProvider.getEntity('counties'))
+            );
+
+        admin.menu(menu);
+
+        adminProvider.setConfiguration(admin);
+    });
