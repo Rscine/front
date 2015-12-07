1e56b21adcbd115dfc45d12f29eaf6633dfd8f3e
monzey <maxi.bertrand@gmail.com>
2015-12-07 20:30:26 +0100

    changed repo name

--- app/scripts/config/admin/users/requestInterceptor.config.js
+++ app/scripts/config/admin/users/requestInterceptor.config.js
@@ -15,12 +15,20 @@ angular.module('rscineFrontendApp')
 
                 if(operation === 'patch' || operation === 'put') {
                     delete element.id; // On supprime l'id (qui n'est pas dans le formulaire de l'api)
+                    delete element.company_id;
+                    delete element.contact_emails;
+                    delete element.contact_phones;
+                    // transform department_id to department
+                    element.department = element.department_id;
                     delete element.department_id;
                     element = { appbundle_user_profile: element };
                 }
 
                 if(operation === 'post') {
                     delete element.id; // On supprime l'id (qui n'est pas dans le formulaire de l'api)
+                    delete element.department_id;
+                    delete element.contact_emails;
+                    delete element.contact_phones;
                     delete element.department_id;
                     element.plainPassword = {first: element.firstPlainPassword, second: element.secondPlainPassword};
                     delete element.firstPlainPassword;
